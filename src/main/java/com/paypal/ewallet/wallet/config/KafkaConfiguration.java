97426ccb0e2f6c4a04eb9684b893daff7e93b939
SaiSandeepGollapudi <gsandeep2406@gmail.com>
2024-06-02 14:58:40 -0700

    eWalletWalletService

--- src/main/java/com/paypal/ewallet/wallet/config/KafkaConfiguration.java
+++ src/main/java/com/paypal/ewallet/wallet/config/KafkaConfiguration.java
@@ -1,4 +1,22 @@
 package com.paypal.ewallet.wallet.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.Map;
+
+@Configuration
 public class KafkaConfiguration {
+
+    @Bean
+    public ConsumerFactory<String,String> consumerFactory(){
+        return new DefaultKafkaConsumerFactory<>(consumerConfig());
+    }
+
+    private Map<String, Object> consumerConfig() {
+        Map<String,>
+    }
+
 }
